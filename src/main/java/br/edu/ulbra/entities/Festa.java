6f62e1e5dbc9848af052a4023f4d4e988fd6428a
joaomanoel333 <joaomanoel333@gmail.com>
2017-05-31 13:14:52 -0300

    Teste

---
+++ src/main/java/br/edu/ulbra/entities/Festa.java
@@ -0,0 +1,24 @@
+package br.edu.ulbra.entities;
+
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Festa {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "nome", nullable = false, length = 200)
+	private String nome;
+}
